3b916e24985f411a228813f24ff727c34c8c8e2d
Dhondi Vinay <144611501+Vinay6313@users.noreply.github.com>
2025-01-18 20:43:00 +0530

    Update web.js

--- web.js
+++ web.js
@@ -1,21 +1,11 @@
-let sendMessage = document.querySelector("#Send-Message");
+let  moreText = document.querySelector("#more-text")
+let  readMoreBtn = document.querySelector("#moreBtn")
 
 
-function toggleText() {
-    const moreText = document.querySelector('.more-text');
-    const button = document.querySelector('.btn');
 
-    if (moreText.style.display === 'none' || moreText.style.display === '') {
-        moreText.style.display = 'inline'; // Show the text
-        button.textContent = 'Read more'; // Change button text
-    } else {
-        moreText.style.display = 'none'; // Hide the text
-        button.textContent = 'Read less'; // Revert button text
-    }
-}
-
-sendMessage.addEventListener ("click", () => {
-    alert("Your response is submitted")
+readMoreBtn.addEventListener("click", ()=>{
+    moreText.style.display = "block";
+    readMoreBtn.textContent ="End";
 })
 
 
